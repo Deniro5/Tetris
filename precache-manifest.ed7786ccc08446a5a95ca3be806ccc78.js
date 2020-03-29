9e007e688f976c091baac046081c77e1755fc0a5
USER <user@USERs-MacBook-Pro.local>
2020-03-29 15:47:38 -0400

    Updates

---
+++ precache-manifest.ed7786ccc08446a5a95ca3be806ccc78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80bcd2e965b3e588d380",
+    "url": "/Tetris/./static/js/runtime~main.80bcd2e9.js"
+  },
+  {
+    "revision": "ae835606f9a6539a6675",
+    "url": "/Tetris/./static/js/main.ae835606.chunk.js"
+  },
+  {
+    "revision": "0ef2f036398c3b3fd4b0",
+    "url": "/Tetris/./static/js/1.0ef2f036.chunk.js"
+  },
+  {
+    "revision": "ae835606f9a6539a6675",
+    "url": "/Tetris/./static/css/main.d61db242.chunk.css"
+  },
+  {
+    "revision": "6f5e7855c4d243fe5bf0b9aa36a09315",
+    "url": "/Tetris/./index.html"
+  }
+];
